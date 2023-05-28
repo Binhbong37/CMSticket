740ac2acd0398c32bcb3d1654e63b69713178e69
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2023-05-28 22:17:41 +0700

    Tạo search filter

---
+++ src/Components/SearchFilter.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../assets/css/searchFilter.css'
+import { AiOutlineSearch } from "react-icons/ai";
+import { FiFilter } from 'react-icons/fi'
+
+const SearchFilter = () => {
+  return (
+    <div className='search-filter'>
+      <div className='search'>
+        <input type="text" placeholder='Tìm bằng số vé' />
+        <AiOutlineSearch />
+      </div>
+      <div className='filter'>
+        <button className='btn btn-filter'>
+          <FiFilter /> Lọc vé
+        </button>
+        <button className="btn btn-filter">Xuất file (.csv)</button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchFilter
--- src/pages/dashboard/QuanLyVe.tsx
+++ src/pages/dashboard/QuanLyVe.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import SearchFilter from '../../Components/SearchFilter'
 
 const QuanLyVe = () => {
   return (
     <div>
       <h2 className='title'>Danh sách vé</h2>
+      <div className=''>
+        <SearchFilter />
+      </div>
+
     </div>
   )
 }
